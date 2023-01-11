b74e2da9a5f7e29e96a774f0a3b53932d0eefd57
Gregor Voigts <gregor.voigts@gmail.com>
2023-01-11 09:51:19 +0100

    init

---
+++ frontend/lib/frontend.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library frontend;
+
+export 'src/frontend_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
